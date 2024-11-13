8cda74e6ecb504d1cdf4050d5da5881a91f3fc33
suman shah <sumanshahofficial@gmail.com>
2024-11-13 14:08:35 +1100

    final

--- gif.py
+++ gif.py
@@ -1,1 +1,10 @@
-print("hello world")
+import imageio.v3 as iio
+#create list of ltwo images
+filenames = ['team-pic1.png', 'team-pic2.png']
+#lets create an empty list which will hold our image data
+images = [ ]
+#for loop to fo through file path and read images using imread methode
+for filename in filenames:
+    images.append(iio.imread(filename))
+#lets run imerite methode to write gif
+iio.imwrite('team.gif', images, duration = 500, loop =0)
